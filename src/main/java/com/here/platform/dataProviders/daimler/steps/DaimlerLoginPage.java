200a9b2e4de3cd3305b0030779b18bdbd9c7cff4
marinsky <ext-roman.marinskyi@here.com>
2020-11-09 17:42:35 +0200

    Fixed Daimler OAUTH login loading problems

--- src/main/java/com/here/platform/dataProviders/daimler/steps/DaimlerLoginPage.java
+++ src/main/java/com/here/platform/dataProviders/daimler/steps/DaimlerLoginPage.java
@@ -2,45 +2,46 @@ package com.here.platform.dataProviders.daimler.steps;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.sleep;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.here.platform.common.DataSubject;
 import io.qameta.allure.Step;
-import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+import org.openqa.selenium.By;
 
 
 @UtilityClass
 public class DaimlerLoginPage {
 
+    private final By loginPageSelector = By.id("loginPage");
+
     @Step("Login data subject on Mercedes.me site")
     public void loginDataSubjectOnDaimlerSite(DataSubject dataSubject) {
-        sleep(3000); //hotfix cos of FE developer rotation
+        $(loginPageSelector).waitUntil(Condition.visible, 20000);
         $("#username").setValue(dataSubject.getEmail());
         $("#continue").click();
+        waitForPreLoaderDisapears();
         $("#login-with-password").click();
         $("#password").setValue(dataSubject.getPass());
         $("#confirm").click();
+        waitForPreLoaderDisapears();
     }
 
-    @Step("Login data subject on Mercedes.me site")
-    public void loginDataSubjectOnDaimlerSiteOld(DataSubject dataSubject) {
-        sleep(3000); //hotfix cos of FE developer rotation
-        $("#name").setValue(dataSubject.getEmail());
-        $("#password").setValue(dataSubject.getPass());
-        $("#ciam-weblogin-auth-login-button").click();
+    private void waitForPreLoaderDisapears() {
+        $("#loader").waitUntil(Condition.disappear, 10000);
     }
 
     @Step("Accept consent scopes")
-    @SneakyThrows
     public void approveDaimlerLegalAndSubmit() {
         if (!$("#allow").isDisplayed()) {
             for (SelenideElement scope : $$("#legaltext0")) {
                 scope.click();
             }
-            Thread.sleep(1000);
+            Selenide.sleep(1000);
             $("#continue").click();
+            waitForPreLoaderDisapears();
         }
     }
 
@@ -48,14 +49,7 @@ public class DaimlerLoginPage {
     public void approveDaimlerScopesAndSubmit() {
         //no need to enable scopes, all of tem enabled by default
         $("#allow").click();
-    }
-
-    @Step("Accept consent scopes")
-    public void approveDaimlerScopesAndSubmitOld() {
-        for (SelenideElement scope : $$("[name*='scope:mb']")) {
-            scope.click();
-        }
-        $("#consent-btn").click();
+        waitForPreLoaderDisapears();
     }
 
 }
