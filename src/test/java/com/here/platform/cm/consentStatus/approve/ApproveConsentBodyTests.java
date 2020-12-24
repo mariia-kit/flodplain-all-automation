09c97dff30362925f80fb31718b94b3434a2d58c
Valerii Sushkov <vsushkov@intellias.com>
2020-12-24 15:28:30 +0200

    Fix policy link test.

--- src/test/java/com/here/platform/cm/consentStatus/approve/ApproveConsentBodyTests.java
+++ src/test/java/com/here/platform/cm/consentStatus/approve/ApproveConsentBodyTests.java
@@ -9,16 +9,20 @@ import com.here.platform.cm.enums.ProviderApplications;
 import com.here.platform.cm.rest.model.ConsentInfo;
 import com.here.platform.cm.rest.model.ConsentInfo.StateEnum;
 import com.here.platform.cm.rest.model.SuccessApproveData;
+import com.here.platform.cm.steps.api.ConsentFlowSteps;
 import com.here.platform.cm.steps.api.ConsentRequestSteps2;
 import com.here.platform.cm.steps.api.RemoveEntitiesSteps;
+import com.here.platform.common.DataSubject;
 import com.here.platform.common.ResponseAssertion;
 import com.here.platform.common.ResponseExpectMessages.StatusCode;
 import com.here.platform.common.annotations.CMFeatures.ApproveConsent;
 import com.here.platform.common.strings.VIN;
 import com.here.platform.dataProviders.daimler.DataSubjects;
 import com.here.platform.dataProviders.reference.controllers.ReferenceTokenController;
+import com.here.platform.hereAccount.controllers.HereUserManagerController.HereUser;
 import com.here.platform.ns.dto.User;
 import com.here.platform.ns.dto.Users;
+import com.here.platform.ns.helpers.authentication.AuthController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,20 +36,41 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 
 
 @ApproveConsent
-@DisplayName("Approve consent")
+@DisplayName("Approve consent Pending Consent list")
 @Execution(ExecutionMode.SAME_THREAD)
 public class ApproveConsentBodyTests extends BaseConsentStatusTests {
+    ProviderApplications targetApp = ProviderApplications.REFERENCE_CONS_1;
+    HereUser hereUser = null;
+    DataSubject dataSubjectIm;
+
+    @BeforeEach
+    void createConsentRequestAndApproveConsent() {
+
+        hereUser = new HereUser(faker.internet().emailAddress(), faker.internet().password(), "here");
+        dataSubjectIm = new DataSubject(
+                hereUser.getEmail(),
+                hereUser.getPassword(),
+                VIN.generate(targetApp.getProvider().getVinLength())
+        );
+        hereUserManagerController.createHereUser(hereUser);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        if (hereUser != null) {
+            hereUserManagerController.deleteHereUser(hereUser);
+        }
+        AuthController.deleteToken(dataSubjectIm);
+    }
 
     @Test
     @DisplayName("Approve consent with single pending consent")
     void approveConsentWithSinglePendingConsentTest() {
-        ProviderApplications targetApp = ProviderApplications.REFERENCE_CONS_1;
         User mpConsumer = Users.MP_CONSUMER.getUser();
         ConsentRequestContainer targetContainer1 = ConsentRequestContainers.generateNew(targetApp.getProvider());
         ConsentRequestContainer targetContainer2 = ConsentRequestContainers.generateNew(targetApp.getProvider());
 
-        DataSubjects dataSubject = DataSubjects.getNextVinLength(targetApp.getProvider().getVinLength());
-        String vinToApprove = dataSubject.getVin();
+        String vinToApprove = dataSubjectIm.getVin();
 
         ConsentInfo consentInfo1 = Consents.generateNewConsentInfo(mpConsumer, targetContainer1);
         ConsentInfo consentInfo2 = Consents.generateNewConsentInfo(mpConsumer, targetContainer2);
@@ -71,7 +96,7 @@ public class ApproveConsentBodyTests extends BaseConsentStatusTests {
 
         var approveConsentResponse = consentStatusController
                 .withConsumerToken()
-                .approveConsent(consentToApprove, dataSubject.getBearerToken());
+                .approveConsent(consentToApprove, AuthController.getDataSubjectToken(dataSubjectIm));
 
         var successApproveData = new ResponseAssertion(approveConsentResponse)
                 .statusCodeIsEqualTo(StatusCode.OK)
