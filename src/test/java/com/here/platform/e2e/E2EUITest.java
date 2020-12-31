31072fbb05892e593b88a4eeaf495a118b2f4730
Valerii Sushkov <vsushkov@intellias.com>
2020-12-31 14:36:23 +0200

    Typo hot fix

--- src/test/java/com/here/platform/e2e/E2EUITest.java
+++ src/test/java/com/here/platform/e2e/E2EUITest.java
@@ -42,6 +42,7 @@ import java.util.logging.Level;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
