825aaf19015571b4ffe97f204b54932166c31d9f
mkit <mkit@intellias.com>
2021-01-25 14:14:52 +0200

    AddVinTestFix Added validation if user added the same vin again

--- src/main/java/com/here/platform/cm/pages/VINEnteringPage.java
+++ src/main/java/com/here/platform/cm/pages/VINEnteringPage.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.sleep;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 
 
 public class VINEnteringPage extends BaseCMPage {
@@ -26,5 +27,16 @@ public class VINEnteringPage extends BaseCMPage {
         $(byText("Continue")).click();
         this.vinNumberInput.waitUntil(Condition.hidden, 10000);
     }
+
+    @Step("Verify if provided VIN already used by this account")
+    public void verifyUsedVinError(String vin){
+        this.vinNumberInput.setValue(vin);
+        sleep(1000);
+        $(byText("Continue")).click();
+        $(By.cssSelector(".ng-star-inserted p:only-child"))
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Provided VIN already used by this account"));
+    }
+
 
 }
