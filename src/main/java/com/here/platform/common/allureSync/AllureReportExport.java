137385a1af3311b38717fa104090562943c12b34
Valerii Sushkov <vsushkov@intellias.com>
2021-01-26 12:34:12 +0200

    Add fe task.

--- src/main/java/com/here/platform/common/allureSync/AllureReportExport.java
+++ src/main/java/com/here/platform/common/allureSync/AllureReportExport.java
@@ -23,6 +23,8 @@ public class AllureReportExport {
         String env = System.getProperty("env");
         System.out.println("Current env: " + env);
         System.out.println("Current ci: " + System.getProperty("ci"));
+        System.out.println("Current IMAGE_TAG: " + System.getProperty("IMAGE_TAG"));
+        System.out.println("Current SERVICE: " + System.getProperty("SERVICE"));
         String project_id = System.getProperty("allure_project");
         String executionName = System.getenv("CI_PIPELINE_ID");
         System.out.println("Current allure proj: " + project_id);
